a0797739977c959f5f88ab37a0c1d73860138d7b
jinhyenkim01 <jinhyenkim01@gmail.com>
2025-10-20 17:55:05 +0900

    feat($Application): implement HashMap
    Implemented HashMap that stores the separator charaters.
    Initialized so that ',' and ':' are recognized as separators.
    Also changed user input retrieval to use camp.nextstep.edu.missionutils.Console.

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,12 +1,18 @@
 package calculator;
 
-import java.util.Scanner;
+import camp.nextstep.edu.missionutils.Console;
+import java.util.HashMap;
 
 public class Application {
     public static void main(String[] args) {
+        HashMap<Character, Boolean> separatorChar = new HashMap<Character, Boolean>();
+        separatorChar.put(',', true);
+        separatorChar.put(':', true);
+
         System.out.println("덧셈할 문자열을 입력해 주세요.");
-        Scanner reader = new Scanner(System.in);
-        String userName = reader.nextLine();
-        reader.close();
+
+        String input = Console.readLine();
+
+        System.out.println(input);
     }
 }
